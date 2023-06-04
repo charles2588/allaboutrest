33cdaed0e500fb198072a3eede7d4cc49c873d0d
chuck <charles2588@gmail.com>
2023-06-04 12:26:42 -0700

    initial commit

---
+++ allaboutrestapis/src/main/java/com/aminnovent/allaboutrestapis/AllaboutrestapisApplication.java
@@ -0,0 +1,13 @@
+package com.aminnovent.allaboutrestapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllaboutrestapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllaboutrestapisApplication.class, args);
+	}
+
+}
---
+++ allaboutrestapis/src/test/java/com/aminnovent/allaboutrestapis/AllaboutrestapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aminnovent.allaboutrestapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllaboutrestapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
